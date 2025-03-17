5a99e31e1b4261dc02cbf6b382b03b66ae7ff461
Moses Oghene <mosesogheneo@gmail.com>
2025-03-17 05:18:03 +0100

    Database created and connected

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(helmet())
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Database connection established")
+}).catch(err => console.log(err))
+
 app.get("/", (req, res) => {
     res.json({message: "Hello world from the server"});
 });
