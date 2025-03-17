07ec7bc2c049b61ff42399743d9ca5bf7d530a91
Moses Oghene <mosesogheneo@gmail.com>
2025-03-17 05:01:50 +0100

    Imported all required modules

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello World")
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cors());
+app.use(helmet())
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.get("/", (req, res) => {
+    res.json({message: "Hello world from the server"});
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening...")
+})
